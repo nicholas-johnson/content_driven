26ee4caed7df9ae3849080bef2a4912adf460008
Nicholas Johnson <nicholas@forwardadvance.com>
2013-10-14 10:41:22 +0100

    Moves some TODO comments into the NodeList file where they will be implemented.

--- lib/sodium/blog.rb
+++ lib/sodium/blog.rb
@@ -3,23 +3,7 @@ require_relative 'node'
 module Sodium
 
   class Blog < Node
-
-    # def last_blog_post
-    #   blog_posts.values.last
-    # end
-
-    # def next_blog_post key
-    #   keys = post.keys
-    #   self.posts[keys[keys.index_of(key) + 1]]
-    # end
-
-    # def prev_blog_post key
-    #   keys = post.keys
-    #   self.posts[keys[keys.index_of(key) + 1]]
-    # end
-
-
-    # Convenience shorthand methods
+    # TODO: Convenience shorthand methods
     # alias_method :add_post,  :add_blog_post
     # alias_method :next_post, :next_blog_post
     # alias_method :prev_post, :prev_blog_post
--- lib/sodium/node_list.rb
+++ lib/sodium/node_list.rb
@@ -1,5 +1,19 @@
 module Sodium
   class NodeList < Hash
 
+    # TODO: Add methods like this:
+    # def last_blog_post
+    #   blog_posts.values.last
+    # end
+
+    # def next_blog_post key
+    #   keys = post.keys
+    #   self.posts[keys[keys.index_of(key) + 1]]
+    # end
+
+    # def prev_blog_post key
+    #   keys = post.keys
+    #   self.posts[keys[keys.index_of(key) + 1]]
+    # end
   end
 end
