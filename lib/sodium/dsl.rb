0550e08afb71b60732fcbe41e9e884d0ec58f70c
Nicholas Johnson <nicholas@forwardadvance.com>
2013-10-14 10:31:23 +0100

    Extracts DSL into a module

---
+++ lib/sodium/dsl.rb
@@ -0,0 +1,32 @@
+require 'active_support/core_ext/string'
+require_relative "content_types"
+
+module Sodium
+  # Extends Sodium with methods to create content of all the types it knows about
+  module DSL
+
+    # Dynamically extend the DSL when we subclass Node
+    def define_dsl_for(subclass)
+      class_name = subclass.to_s.split("::").last.camelcase.underscore
+
+      # Allows us to call methods like site.blogs and blog.blog_posts
+      define_method("get_#{class_name.pluralise}") do
+        children.dup.delete_if do |child|
+          !children[child].is_a? subclass
+        end
+      end
+
+      # Allows us to call methods like site.get_blog which will return the first blog
+      define_method("get_#{class_name}") do |url=nil|
+        send("get_#{class_name.pluralise}")[url]
+      end
+
+      # Allows us to call methods like site.add_blog and blog.add_blog_posts
+      define_method "add_#{class_name}" do |key=nil, &block|
+        content_class = Sodium.const_get class_name.to_s.camelcase
+        child = content_class.new &block
+        add_child(key, child)
+      end
+    end
+  end
+end
--- sodium.gemspec
+++ sodium.gemspec
@@ -8,7 +8,7 @@ Gem::Specification.new do |spec|
   spec.version       = Sodium::VERSION
   spec.authors       = ["Nicholas Johnson"]
   spec.email         = ["nicholas@forwardadvance.com"]
-  spec.description   = %q{A simple DSL for defining complete websites}
+  spec.description   = %q{A simple DSL for defining websites}
   spec.summary       = %q{Create your website in HAML, ERB, MD, etc}
   spec.homepage      = ""
   spec.license       = "MIT"
@@ -20,4 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
+  spec.add_dependency "activesupport"
 end
